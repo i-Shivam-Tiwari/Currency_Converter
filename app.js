4cdf2141e4b93cbd4c079ab86ba68f8657cac816
shivam <st3737888@gmail.com>
2024-07-17 00:22:23 +0530

    Adding flag load on change event

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var BASE_URL = 'https://flagsapi.com'
+
 function updateFromCurr(){
     let fromCurrencySelect = document.getElementById('fromCurrencyList');
     console.log("From currency list" + fromCurrencySelect)
@@ -26,5 +28,37 @@ function updateToCurr(){
     }
 }
 
-updateFromCurr();
-updateToCurr();
+document.addEventListener('DOMContentLoaded', () => {
+    updateFromCurr();
+    updateToCurr();
+    updateFromCountryFlag(document.getElementById('fromCurrencyList').value);
+    updateToCountryFlag(document.getElementById('toCurrencyList').value);
+})
+
+document.getElementById('fromCurrencyList').addEventListener('change', () => {
+    updateFromCountryFlag(document.getElementById('fromCurrencyList').value);
+});
+
+document.getElementById('toCurrencyList').addEventListener('change', () => {
+    updateToCountryFlag(document.getElementById('toCurrencyList').value);
+});
+
+function updateFromCountryFlag(countryCode){
+    let fromCountryFlag = document.getElementById('fromCountryFlag');
+    console.log(fromCountryFlag);
+    fromCountryFlag.src=`${BASE_URL}/${countryCode}/flat/32.png`;
+}
+
+function updateToCountryFlag(countryCode){
+    let toCountryFlag = document.getElementById('toCountryFlag');
+    console.log(toCountryFlag);
+    toCountryFlag.src=`${BASE_URL}/${countryCode}/flat/32.png`;
+}
+
+document.querySelector('.button').addEventListener('click', () => {
+    getConversion(document.getElementById('amount').value, document.getElementById('fromCurrencyList').value, document.getElementById('toCurrencyList').value)
+})
+
+function getConversion(amount, fromCurrency, toCurrency){
+    
+}
